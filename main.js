608f9eee9fcfaa82616ced0eb66508f39687ccc5
124msalvani <124msalvani@frhsd.com>
2024-02-23 09:23:03 -0500

    Completed Lesson 3

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
     data () {
         return{
             product: "Socks",
-            description: "Fluffy warm socks!"
+            description: "Fluffy warm socks!",
+            image: './assets/images/socks_green.jpg',
+            url: "https://www.vuemastery.com"
         }
     }
 })
