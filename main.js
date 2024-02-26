68eca0c14818142159d8b779c4f35757e7687aa9
124msalvani <124msalvani@frhsd.com>
2024-02-26 10:40:44 -0500

    Completed Lesson 6

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
     data () {
         return{
+            cart:0,
             product: "Socks",
             description: "Fluffy warm socks!",
             image: './assets/images/socks_green.jpg',
@@ -9,10 +10,26 @@ const app = Vue.createApp({
             onSale: false,
             details: ["50 cotton", "30% wool", "20% polyester"],
             variants:[
-                { id: 2234, color: 'green'},
-                { id: 2235, color: 'blue'}
+                { id: 2234, color: 'green', image: "./assets/images/socks_green.jpg"},
+                { id: 2235, color: 'blue', image: "./assets/images/socks_blue.jpg"}
             ],
             sizes: ["xsmall", "small", "medium", "large", "xlarge"],
+        }
+    },
+    methods:{
+        addToCart(){
+            this.cart +=1;
+        },
+        updateImage(variantImage){
+            this.image = variantImage;
+        },
+        removeFromCart(){
+            if (this.cart>0){
+                this.cart -=1;
+            } else{
+                this.cart = 0;
+            }
+           
         }
     }
 })
