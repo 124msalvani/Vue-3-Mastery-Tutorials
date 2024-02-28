2c171be3281701798db32cd74fb1f2d1b41710ce
124msalvani <124msalvani@frhsd.com>
2024-02-28 08:36:33 -0500

    Finished Lesson 8

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ const app = Vue.createApp({
             brand: "Vue Mastery",
             product: "Socks",
             description: "Fluffy warm socks!",
-            image: './assets/images/socks_green.jpg',
+            selectedVariant: 0,
             url: "https://www.vuemastery.com",
             inventory: 0,
-            onSale: false,
+            onSale: true,
             details: ["50 cotton", "30% wool", "20% polyester"],
             variants:[
                 { id: 2234, color: 'green', image: "./assets/images/socks_green.jpg", quantity: 50},
@@ -21,8 +21,9 @@ const app = Vue.createApp({
         addToCart(){
             this.cart +=1;
         },
-        updateImage(variantImage){
-            this.image = variantImage;
+        updateVariant(index){
+            this.selectedVariant = index;
+          
         },
         removeFromCart(){
             if (this.cart>0){
@@ -38,7 +39,11 @@ const app = Vue.createApp({
             return this.brand + " " + this.product;
         },
         inStock(){
-            return this.inventory > 0;
+            return this.variants[this.selectedVariant].quantity;
+        },
+        image(){
+            return this.variants[this.selectedVariant].image;
         }
+      
     }
 })
