ded653efc56528f0595a7a08c574037dbdcb8245
124msalvani <124msalvani@frhsd.com>
2024-02-28 08:16:32 -0500

    End of Lesson 7

--- main.js
+++ main.js
@@ -2,17 +2,17 @@ const app = Vue.createApp({
     data () {
         return{
             cart:0,
+            brand: "Vue Mastery",
             product: "Socks",
             description: "Fluffy warm socks!",
             image: './assets/images/socks_green.jpg',
             url: "https://www.vuemastery.com",
-           // inventory: 100,
-            inStock: true,
+            inventory: 0,
             onSale: false,
             details: ["50 cotton", "30% wool", "20% polyester"],
             variants:[
-                { id: 2234, color: 'green', image: "./assets/images/socks_green.jpg"},
-                { id: 2235, color: 'blue', image: "./assets/images/socks_blue.jpg"}
+                { id: 2234, color: 'green', image: "./assets/images/socks_green.jpg", quantity: 50},
+                { id: 2235, color: 'blue', image: "./assets/images/socks_blue.jpg", quantity: 0}
             ],
             sizes: ["xsmall", "small", "medium", "large", "xlarge"],
         }
@@ -32,5 +32,13 @@ const app = Vue.createApp({
             }
            
         },
+    }, 
+    computed:{
+        title(){
+            return this.brand + " " + this.product;
+        },
+        inStock(){
+            return this.inventory > 0;
+        }
     }
 })
